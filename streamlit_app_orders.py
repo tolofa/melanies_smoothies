f6ed324aeba52d5341f051c12caf8b0d9197cef4
tolofa <50927573+tolofa@users.noreply.github.com>
2025-02-06 17:12:56 -0700

    Update streamlit_app_orders.py

--- streamlit_app_orders.py
+++ streamlit_app_orders.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
 # Write directly to the app
@@ -10,7 +10,8 @@ st.write(
     """
 )
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
